7453c0781478a39fa7d80ad2ecc29037be2fe0ed
umair <umairjutt2025@gmail.com>
2023-09-14 17:57:54 +0500

    done

--- index.js
+++ index.js
@@ -8,14 +8,14 @@ app.use(express.json());
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
-app.get('/form', function(req,res) {
+app.get('/', function(req,res) {
     res.render('Form')
 })
 
 const studentapi= require('./Route/Student');
 app.use("/student/api",studentapi)
 
-mongoose.connect("mongodb+srv://umairjutt2025:umairjutt2025@umair-cluster.oducycs.mongodb.net/StudentForm?retryWrites=true&w=majority/").then((res)=>{
+mongoose.connect("mongodb+srv://umairjutt2025:umairjutt2025@umair-cluster.oducycs.mongodb.net/Hello?retryWrites=true&w=majority/").then((res)=>{
     console.log("DataBase is connected");
 }).catch((e)=>{
     console.log(e.message)
